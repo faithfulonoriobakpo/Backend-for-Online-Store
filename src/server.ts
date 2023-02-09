7ff80ef2354f3a32fd0b1a427eec2dab73d3e700
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-09 22:48:36 +0100

    modified app.listen to use arrow function

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+
+app.listen(3000, () => console.log(`starting app on: ${address}`))
