7a92550b29b71d42f4b67aac8ccb47c3a54dd626
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-10 17:27:07 +0100

    Set up database connection file and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address: string = "0.0.0.0:3000";
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-
-
-app.listen(3000, () => console.log(`starting app on: ${address}`))
+app.listen(3000, () => console.log(`starting app on: ${address}`));
