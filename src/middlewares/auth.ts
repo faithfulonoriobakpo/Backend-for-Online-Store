ad2081922381681b7b436d2bd493b1ca093c62ed
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-22 23:42:34 +0100

    Created middleware dir and jwt authentication middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import {NextFunction, Request, Response} from "express";
+
+// Middleware function to authenticate requests
+export const authenticate = (req:Request, res:Response, next:NextFunction) => {
+    // get token from the headers
+    const authorization = req.headers.authorization;
+    const token = req.body.token || req.query.token || req.params.token || authorization?.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({status:401, message:"Access Denied. No Token Provided."});
+    }
+  
+    try {
+      jwt.verify(token, process.env.JWT_SECRET as string);
+      next();
+    } catch (ex) {
+      return res.status(401).json({status:401, message:"Access Denied. Invalid Token Provided."});
+    }
+};
