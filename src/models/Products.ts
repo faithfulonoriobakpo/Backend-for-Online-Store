31c0f61526e57542847f6e0ea46711d23940dde6
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-26 22:08:24 +0100

    refactored products model methods

--- src/models/Products.ts
+++ src/models/Products.ts
@@ -1,11 +1,5 @@
 import Client from '../database';
 
-type Response = {
-    status: number;
-    message: string;
-    data?:string | number | product | product[]
-};
-
 type product = {
     id?: number;
     name: string;
@@ -14,53 +8,31 @@ type product = {
 };
 
 class Product {
-    public async index(name: string): Promise<Response> {
+    public async index(): Promise<product[]> {
         try {
             const conn = await Client.connect();
-            const query = 'SELECT id FROM products WHERE LOWER(name)=$1';
-            const result = await conn.query(query, [name.toLowerCase()]);
+            const query = 'SELECT * FROM products';
+            const result = await conn.query(query);
             conn.release();
-            if (result.rows.length) {
-                return {
-                    status: 200,
-                    message: "Index retrieved successfully",
-                    data: result.rows[0]
-                }
-            } else {
-                return {
-                    status: 400,
-                    message: 'No index found for productName given'
-                };
-            }
+            return result.rows;    
         } catch (e) {
             throw Error('Could not get product index');
         }
     }
 
-    public async show(index: number): Promise<Response> {
+    public async show(index: number): Promise<product> {
         try {
             const conn = await Client.connect();
             const query = 'SELECT * FROM products WHERE id=$1';
             const result = await conn.query(query, [index]);
             conn.release();
-            if (result.rows.length) {
-                return {
-                    status: 200,
-                    message: "product retrieved successfully",
-                    data: result.rows[0]
-                }
-            } else {
-                return {
-                    status: 400,
-                    message: 'No product found for index given'
-                };
-            }
+            return result.rows[0];
         } catch (e) {
             throw Error('Could not find product');
         }
     }
 
-    public async create(product: product): Promise<Response> {
+    public async create(product: product): Promise<number> {
         try {
             const conn = await Client.connect();
             const query =
@@ -72,66 +44,31 @@ class Product {
                 0
             ]);
             conn.release();
-            if (result.rows.length) {
-                return {
-                    status: 200,
-                    message: "product created successfully",
-                    data: result.rows[0]
-                }
-            } else {
-                return {
-                    status: 500,
-                    message: 'Could not create product'
-                };
-            }
+            return result.rows[0];
         } catch (e) {
-            throw new Error('Could not created product');
+            throw new Error('Could not create product');
         }
     }
 
-    public async show_products_by_category(
-        category: string
-    ): Promise<Response> {
+    public async show_products_by_category(category: string): Promise<product[]> {
         try {
             const conn = await Client.connect();
             const query = 'SELECT * FROM products WHERE LOWER(category) = $1';
             const result = await conn.query(query, [category.toLowerCase()]);
             conn.release();
-            if (result.rows.length) {
-                return {
-                    status: 200,
-                    message: "products retrieved successfully",
-                    data: result.rows
-                }
-            } else {
-                return {
-                    status: 400,
-                    message: 'product category not found'
-                };
-            }
+            return result.rows;
         } catch (e) {
             throw Error('Could not get category products');
         }
     }
 
-    public async most_popular_products(): Promise<Response> {
+    public async most_popular_products(): Promise<product[]> {
         try {
             const conn = await Client.connect();
             const query = `SELECT * FROM products ORDER BY orders_count DESC LIMIT 5`;
             const result = await conn.query(query);
             conn.release();
-            if (result.rows.length) {
-                return {
-                    status: 200,
-                    message: "products retrieved successfully",
-                    data: result.rows
-                }
-            } else {
-                return {
-                    status: 500,
-                    message: 'Something went wrong internally'
-                };
-            }
+            return result.rows;
         } catch (e) {
             throw Error('Could not get popular products');
         }
