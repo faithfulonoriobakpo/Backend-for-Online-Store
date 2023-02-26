b89c0986fa351f676150aaeda12432743a04663a
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-26 22:07:31 +0100

    refactored products enpoint handlers

--- src/routes/api/products.ts
+++ src/routes/api/products.ts
@@ -4,12 +4,22 @@ import { authenticate } from '../../middlewares/auth';
 
 const productRoute = express.Router();
 
-productRoute.get('/index/:productName', async (req: Request, res: Response) => {
+productRoute.get('/index', async (req: Request, res: Response) => {
     try {
-        const productName: string = req.params.productName;
         const product_instance = new Product();
-        const index = await product_instance.index(productName);
-        res.status(index.status).json(index);
+        const index = await product_instance.index();
+        if (index.length > 0) {
+            res.status(200).json({
+                status: 200,
+                message: "products index retrieved successfully",
+                data:index
+            })
+        } else {
+            res.status(404).json({
+                status: 404,
+                message: 'products not found'
+            });
+        }
     } catch(e){
         res.status(500).json({
             status:500,
@@ -18,13 +28,24 @@ productRoute.get('/index/:productName', async (req: Request, res: Response) => {
     }
 });
 
-productRoute.get('/show/:index', async (req:Request, res:Response) => {
+productRoute.get('/show/:id', async (req:Request, res:Response) => {
     try{
-        const index = Number(req.params.index);
-        if(isNaN(index)) throw new TypeError("index must be a number");
+        const id = Number(req.params.id);
+        if(isNaN(id)) throw new TypeError("product id must be a number");
         const product_instance = new Product();
-        const product = await product_instance.show(index);
-        res.status(product.status).json(product);
+        const product = await product_instance.show(id);
+        if(product){
+            res.status(200).json({
+                status: 200,
+                message: "product retrieved successfully",
+                data: product
+            });
+        }else{
+            res.status(404).json({
+                status: 404,
+                message: 'No product found for id given'
+            });
+        }
     }catch(e){
         if (e instanceof TypeError){
             res.status(400).json({
@@ -51,7 +72,18 @@ productRoute.post('/create', authenticate, async (req:Request, res:Response) => 
             if(isNaN(product.price)) throw new TypeError("Price must be a number");
             const product_instance = new Product();
             const response = await product_instance.create(product);
-            res.status(response.status).json(response);
+            if(response){
+                res.status(200).json({
+                    status: 200,
+                    message: "product created successfully",
+                    data: response
+                });
+            }else{
+                res.status(500).json({
+                    status: 500,
+                    message: 'Could not create product'
+                });
+            }
         }else{
             res.status(400).json({
                 status: 400,
@@ -78,7 +110,18 @@ productRoute.get('/category', async (req:Request, res:Response) => {
         const category:string = req.query.category as string;
         const product_instance = new Product();
         const result = await product_instance.show_products_by_category(category);
-        res.status(result.status).json(result);
+        if (result.length > 0){
+            res.status(200).json({
+                status: 200,
+                message: "products retrieved successfully",
+                data: result
+            });
+        }else{
+            res.status(404).json({
+                status: 404,
+                message: 'product category not found'
+            });
+        }
     }catch(e){
         if(e instanceof Error){
             res.json({
@@ -93,7 +136,18 @@ productRoute.get('/popular', async (req:Request, res:Response) => {
     try{
         const product_instance = new Product();
         const result = await product_instance.most_popular_products();
-        res.status(result.status).json(result);
+        if(result.length > 0) {
+            res.status(200).json({
+                status: 200,
+                message: "products retrieved successfully",
+                data: result
+            });
+        }else{
+            res.status(500).json({
+                status: 500,
+                message: 'Something went wrong internally'
+            });
+        }
     }catch(e){
         if(e instanceof Error){
             res.json({
