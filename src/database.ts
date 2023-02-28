44b3be979f9fb18a30195286d3b31af8562b08de
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-28 19:37:08 +0100

    configured database config file to point to different database based on environment specified in dotenv file

--- src/database.ts
+++ src/database.ts
@@ -6,13 +6,14 @@ dotenv.config();
 const {
     POSTGRES_HOST,
     POSTGRES_DATABASE,
+    POSTGRES_DATABASE_TEST,
     POSTGRES_USERNAME,
     POSTGRES_PASSWORD
 } = process.env;
 
 const Client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DATABASE,
+    database: process.env.ENV == "dev"? POSTGRES_DATABASE: POSTGRES_DATABASE_TEST,
     user: POSTGRES_USERNAME,
     password: POSTGRES_PASSWORD
 });
